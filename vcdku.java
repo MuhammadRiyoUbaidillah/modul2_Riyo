44955e18dc132e3952d5ba361eee0811b63a1737
MuhammadRiyoUbaidillah <82527896+MuhammadRiyoUbaidillah@users.noreply.github.com>
2021-04-14 22:47:17 +0700

    Create vcdku.java

---
+++ vcdku.java
@@ -0,0 +1,17 @@
+package tugasmdl2;
+//superclass
+public class vcdku {
+    public String judul,publisher;
+    int stok;
+    //construktor
+    vcdku(String judul,String publisher, int stok){
+        this.judul=judul;
+        this.publisher=publisher;
+        this.stok=stok;
+    }
+    //overload construktor
+    vcdku(){
+        
+    }
+}
+    
